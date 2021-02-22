5a45b7e86a052b7b00b8d4a327523e0f610be1d4
Nenad Čanić <chanasrb@gmail.com>
2021-02-22 15:04:00 +0100

    sql

--- app/Models/Izbor.php
+++ app/Models/Izbor.php
@@ -29,6 +29,23 @@ class Izbor extends Model
         // Field, Type, Null, Key, Default, Extra
         $sql = "SHOW COLUMNS FROM {$baza}.{$tabela};";
 
+        /*
+        SELECT  INFORMATION_SCHEMA.COLUMNS.TABLE_SCHEMA AS baza, INFORMATION_SCHEMA.COLUMNS.TABLE_NAME AS tabela,
+                INFORMATION_SCHEMA.COLUMNS.COLUMN_NAME AS polje, INFORMATION_SCHEMA.COLUMNS.DATA_TYPE AS tip,
+                INFORMATION_SCHEMA.COLUMNS.CHARACTER_MAXIMUM_LENGTH AS duzina,
+                INFORMATION_SCHEMA.COLUMNS.IS_NULLABLE AS nulabilno, INFORMATION_SCHEMA.COLUMNS.COLUMN_DEFAULT AS povrazumevano,
+                INFORMATION_SCHEMA.COLUMNS.COLUMN_KEY AS kljuc,
+		        veza_od.ref_tabela, veza_od.ref_polje
+        FROM INFORMATION_SCHEMA.COLUMNS
+        LEFT JOIN ( SELECT  INFORMATION_SCHEMA.KEY_COLUMN_USAGE.COLUMN_NAME,
+                            INFORMATION_SCHEMA.KEY_COLUMN_USAGE.REFERENCED_TABLE_NAME AS ref_tabela,
+                            INFORMATION_SCHEMA.KEY_COLUMN_USAGE.REFERENCED_COLUMN_NAME AS ref_polje
+                    FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+                    WHERE REFERENCED_TABLE_SCHEMA = 'portal' AND TABLE_NAME = 'clanci') AS veza_od
+        ON `INFORMATION_SCHEMA`.`COLUMNS`.`COLUMN_NAME` = veza_od.COLUMN_NAME
+        WHERE INFORMATION_SCHEMA.COLUMNS.TABLE_SCHEMA = 'portal' AND INFORMATION_SCHEMA.COLUMNS.TABLE_NAME = 'clanci'
+        */
+
         return $this->fetch($sql);
     }
 
